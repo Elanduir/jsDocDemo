20a27e9488a5a0cd6efdd4279bec9e1cf8543853
Mauro Hirt <mauro.hirt@uzh.ch>
2023-03-12 15:12:46 +0100

    Class def and extending a class

--- src/script2.js
+++ src/script2.js
@@ -28,7 +28,7 @@ console.log(foo)
 
 /** @type {(number|boolean)}*/
 var abc;
-foo = [];
+foo = []; // marked as wrong
 
 // nullable non nullable types {?number} {!number}
 
@@ -77,4 +77,73 @@ function foo() {
 function foo(n, o, d) {
     return n
   }
+
+// Simple class
+
+/** Class representing a point. */
+const Point = class {
+    /**
+     * Create a point.
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     */
+    constructor(x, y) {
+        // ...
+        self.x = x;
+        self.y = y;
+    }
+
+    /**
+     * Get the x value.
+     * @return {number} The x value.
+     */
+    getX() {
+        // ...
+    }
+
+    /**
+     * Get the y value.
+     * @return {number} The y value.
+     */
+    getY() {
+        // ...
+    }
+
+    /**
+     * Convert a string containing two comma-separated numbers into a point.
+     * @param {string} str - The string containing two comma-separated numbers.
+     * @return {Point} A Point object.
+     */
+    static fromString(str) {
+        // ...
+    }
+}
+// also @implements , @interface, @ other important tags
+// Extending classes
+
+/**
+ * Class representing a dot.
+ * @extends Point
+ */
+class Dot extends Point {
+    /**
+     * Create a dot.
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     * @param {number} width - The width of the dot, in pixels.
+     */
+    constructor(x, y, width) {
+        super(x, y);
+
+        // ...
+    }
+
+    /**
+     * Get the dot's width.
+     * @return {number} The dot's width, in pixels.
+     */
+    getWidth() {
+        // ...
+    }
+}
 
